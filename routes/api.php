cbe2ed58ba2d7447c58fbedafee6972d0f7ca868
mohamed <amalkirifi01@gmail.com>
2026-05-05 16:03:13 +0200

    create api with auth

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login',    [AuthController::class, 'login']);
+Route::get('/products',       [ProductController::class, 'index']);
+Route::get('/products/{id}',  [ProductController::class, 'show']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::post('/products',         [ProductController::class, 'store']);
+    Route::put('/products/{id}',     [ProductController::class, 'update']);
+    Route::delete('/products/{id}',  [ProductController::class, 'destroy']);
+});
